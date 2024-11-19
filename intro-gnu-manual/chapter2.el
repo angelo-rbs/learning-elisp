0dd24ec3b91e8159a0018941d7c718bed089f96b
angelo-rbs <mesvras@proton.me>
2024-11-18 23:42:30 -0300

    att: chapter 2 exercises

---
+++ intro-gnu-manual/chapter2.el
@@ -0,0 +1,38 @@
+;;; chapter2.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Ângelo Barbosa
+;;
+;; Author: Ângelo Barbosa <angeloraphael1@hotmail.com>
+;; Maintainer: Ângelo Barbosa <angeloraphael1@hotmail.com>
+;; Created: novembro 18, 2024
+;; Modified: novembro 18, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/angelo/chapter2
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+;; buffer names
+(buffer-name)
+buffer-file-name
+(buffer-file-name)
+
+;; buffer-file-name is an symbol that refers to both a function and a variable
+
+(current-buffer)
+(other-buffer)
+
+(buffer-size)
+(point)
+(point-max)
+(point-min)
+
+(provide 'chapter2)
+;;; chapter2.el ends here
